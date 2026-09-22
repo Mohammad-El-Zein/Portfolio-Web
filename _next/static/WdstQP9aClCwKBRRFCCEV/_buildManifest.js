711ee93e62b1928537d2907d739fa4a3dd78207e
Mohammad El Zein <mhmdzein70781311@gmail.com>
2026-09-23 01:33:09 +0200

    Updates

--- _next/static/WdstQP9aClCwKBRRFCCEV/_buildManifest.js
+++ _next/static/WdstQP9aClCwKBRRFCCEV/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Portfolio-Web//_next/:path+",
-        "destination": "/Portfolio-Web/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
